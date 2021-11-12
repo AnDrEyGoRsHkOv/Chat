674cbb81463dddd906627345efbd48c6a2c0c6bb
Андрей <88981892+AnDrEyGoRsHkOv@users.noreply.github.com>
2021-11-12 19:43:49 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>React hello</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
